b4287aeabb918e0e88edea688646ac2efa219d02
CZB666-wdnmd <72345305+CZB666-wdnmd@users.noreply.github.com>
2023-02-02 23:32:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ print('linux需要wget')
 while True:
     u = input('输入问题')
     s = os.name
-    if s == posix:
+    if s == 'posix':
         os.system('wget https://github.com/CZB666-wdnmd/chatczb/raw/main/hklmix_agadfaeaak4wqvy.png -O /tmp/out.png')
         print('回答在/tmp/out.png')
     else:
