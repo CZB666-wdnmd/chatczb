5a2c1437add65e94a4f74fd8bf5965a37a55039d
CZB666-wdnmd <72345305+CZB666-wdnmd@users.noreply.github.com>
2023-02-02 23:27:17 +0800

    final commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+print('linux需要wget')
+while true:
+    u = input('输入问题')
+    s = os.name
+    if s == posix:
+        os.system('wget https://github.com/CZB666-wdnmd/chatczb/raw/main/hklmix_agadfaeaak4wqvy.png -O /tmp/out.png')
+        print('回答在/tmp/out.png')
+    else:
+        os.system('start https://github.com/CZB666-wdnmd/chatczb/raw/main/hklmix_agadfaeaak4wqvy.png')
