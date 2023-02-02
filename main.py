89570850e4ebfbc981255cd22fc1f2416615a313
CZB666-wdnmd <72345305+CZB666-wdnmd@users.noreply.github.com>
2023-02-02 23:35:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import os
 print('linux需要wget')
 while True:
-    u = input('输入问题')
+    u = input('输入问题(ctrl+c退出)')
     s = os.name
     if s == 'posix':
         os.system('wget https://github.com/CZB666-wdnmd/chatczb/raw/main/hklmix_agadfaeaak4wqvy.png -O /tmp/out.png')
         print('回答在/tmp/out.png')
-    else:
+    elif s == 'nt':
         os.system('start https://github.com/CZB666-wdnmd/chatczb/raw/main/hklmix_agadfaeaak4wqvy.png')
+    else:
+        print('不支持的系统')
+        break
